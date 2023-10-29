245fab633a74f3d419f6ee91ea44ca76a808fb3c
Fang Zhili <s2189538@siswa.um.edu.my>
2023-10-29 12:27:51 +0800

    Setting up the layout and basic clicking actions

--- app/src/main/java/com/example/urbancycle/MainActivity.java
+++ app/src/main/java/com/example/urbancycle/MainActivity.java
@@ -1,21 +1,53 @@
 package com.example.urbancycle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.example.urbancycle.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.fragment_container, new MapsFragment())
-                    .commit();
-        }
+        replaceFragment(new MapsFragment());
+        binding.bottomNavigationView.setBackground(null);
+        binding.bottomNavigationView.setSelectedItemId(R.id.map);
+
+        binding.bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+
+
+            int id = item.getItemId();
+            if (id == R.id.map){
+                replaceFragment(new MapsFragment());
+            } else if (id == R.id.community) {
+                replaceFragment(new CommunityFragment());
+            } else if (id == R.id.profile) {
+                replaceFragment(new ProfileFragment());
+            } else if (id == R.id.support) {
+                replaceFragment(new SupportFragment());
+            } else if (id == R.id.reward) {
+                replaceFragment(new RewardFragment());
+            }
+
+            return true;
+        });
     }
 
+    private void replaceFragment(Fragment fragment){
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
 }
