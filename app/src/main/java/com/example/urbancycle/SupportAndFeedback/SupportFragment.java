ee04b1d36fc7dd03b39a36311ee3750ff85a6b00
huang shengzhe <huangshengzhe2004@gmail.com>
2023-11-17 22:59:25 +0800

    feedback implemented 2

--- app/src/main/java/com/example/urbancycle/SupportAndFeedback/SupportFragment.java
+++ app/src/main/java/com/example/urbancycle/SupportAndFeedback/SupportFragment.java
@@ -3,10 +3,12 @@ package com.example.urbancycle.SupportAndFeedback;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.urbancycle.R;
 
@@ -16,5 +18,26 @@ public class SupportFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_support, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        Button feedback=view.findViewById(R.id.feedbackB);
+        View.OnClickListener onFeedback=new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getFragmentManager().beginTransaction().replace(R.id.fragment_container,new Feedback()).addToBackStack(null).commit();
+            }
+        };
+        feedback.setOnClickListener(onFeedback);
+
+        Button FAQ=view.findViewById(R.id.FAQB);
+        View.OnClickListener onFAQ=new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(view).navigate(R.id.DFAQ);
+            }
+        };
+        FAQ.setOnClickListener(onFAQ);
     }
 }
