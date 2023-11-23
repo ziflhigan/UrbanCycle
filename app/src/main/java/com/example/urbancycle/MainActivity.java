0fffe5b5b3cb1c14e98823678099328e0261a654
Fang Zhili <s2189538@siswa.um.edu.my>
2023-11-23 18:18:22 +0800

    Implemented the top tool bar and side drawer, modified some fragments layout to align with the side drawer Fixed the issue of Floating Action Button for 'Direction'

--- app/src/main/java/com/example/urbancycle/MainActivity.java
+++ app/src/main/java/com/example/urbancycle/MainActivity.java
@@ -1,21 +1,29 @@
 package com.example.urbancycle;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 
 
 import com.example.urbancycle.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private DrawerLayout drawerLayout;
     ActivityMainBinding binding;
+    private AppBarConfiguration appBarConfiguration;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,10 +33,22 @@ public class MainActivity extends AppCompatActivity {
         // Setting up the NavController
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
 
+        // Initialize Toolbar
+        Toolbar toolbar = findViewById(R.id.TopActionBar);
+        setSupportActionBar(toolbar);
+
+        // Initialize Drawer Layout
+        drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+
         // Setting up AppBarConfiguration
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+        appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.map, R.id.community, R.id.profile, R.id.support, R.id.reward)
+                .setDrawerLayout(drawerLayout)
                 .build();
+
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
 
         // Setting up the BottomNavigationView with NavController
         NavigationUI.setupWithNavController(binding.bottomNavigationView, navController);
@@ -52,5 +72,28 @@ public class MainActivity extends AppCompatActivity {
     public void onFabDirectionsClicked(View view) {
         // Implement navigation to DirectionsFragment
         Navigation.findNavController(this, R.id.nav_host_fragment).navigate(R.id.directionsFragment);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.side_menu, menu);
+        return true;
     }
 }
