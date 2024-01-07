b1f3b337f59a4b6a9db160c95092fb14a212e588
rexliu2004 <116248319+rexliu2004@users.noreply.github.com>
2024-01-07 20:10:55 +0800

    尽力了

--- app/src/main/java/com/example/urbancycle/Profile/ProfileFragment.java
+++ app/src/main/java/com/example/urbancycle/Profile/ProfileFragment.java
@@ -1,18 +1,22 @@
 package com.example.urbancycle.Profile;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-import androidx.navigation.Navigation;
-
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import com.bumptech.glide.Glide;
 import com.example.urbancycle.Database.ConnectToDatabase;
 import com.example.urbancycle.Database.UserInfoManager;
 import com.example.urbancycle.R;
@@ -28,15 +32,19 @@ import java.util.List;
 public class ProfileFragment extends Fragment implements ConnectToDatabase.DatabaseConnectionListener, RetrieveUserName.UserNameDataListener {
 
  FragmentProfileBinding binding;
+
      String userName = UserInfoManager.getInstance().getUserName();
 
+    private static final int REQUEST_CODE_PICK_IMAGE = 100;
     Connection connection;
+    private ImageView avatarImageView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentProfileBinding.inflate(inflater, container, false);
         View view = binding.getRoot();
-        TextView tv=view.findViewById(R.id.UsernameDisp);
+
+        TextView tv = view.findViewById(R.id.UsernameDisp);
         tv.setText(userName);
 
         binding.History.setOnClickListener(new View.OnClickListener() {
@@ -53,15 +61,49 @@ public class ProfileFragment extends Fragment implements ConnectToDatabase.Datab
                 Navigation.findNavController(view).navigate(R.id.action_profile_to_setting);
             }
         });
-
         binding.Preference.setOnClickListener(new View.OnClickListener() {
-            @Override
             public void onClick(View v) {
-                Navigation.findNavController(view).navigate(R.id.action_profile_to_preference);
+                Navigation.findNavController(view).navigate(R.id.action_profile_to_galleryActivity);
             }
         });
+        avatarImageView = view.findViewById(R.id.Avatar);
+
+        avatarImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startGalleryActivity();
+            }
+        });
+        Glide.with(this)
+                .load(R.drawable.profile)
+                .into(avatarImageView);
 
         return view;
+
+
+
+    }
+    private void startGalleryActivity() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, REQUEST_CODE_PICK_IMAGE);
+
+    }
+
+    // update
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_PICK_IMAGE && resultCode == GalleryActivity.RESULT_OK && data != null) {
+            String selectedImageUri = data.getStringExtra("selectedImageUri");
+            updateAvatar(selectedImageUri);
+        }
+    }
+
+    private void updateAvatar(String selectedImageUri) {
+
+        Glide.with(this)
+                .load(selectedImageUri)
+                .into(avatarImageView);
     }
 
 
@@ -86,6 +128,8 @@ public class ProfileFragment extends Fragment implements ConnectToDatabase.Datab
     public void onConnectionFailure() {
 
     }
+
+
 
 
     @Override
